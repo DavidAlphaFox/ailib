329fe7d28a7ad58456aae645d4d019424954f0fd
David.Gao <david.alpha.fox@gmail.com>
2018-10-25 15:17:08 +0800

    add hash_to_path

--- src/ai_file.erl
+++ src/ai_file.erl
@@ -4,7 +4,9 @@
 -export([files_in_dir/1,priv_dir/2]).
 -export([remove_recursive/1,create_dir/1,create_priv_dir/2]).
 -export([open_for_write/1,open_for_read/1,file_size/1]).
-    
+-export([hash_to_path/2,hash_to_fullname/2]).
+
+
 files_in_dir(Dir) ->
 		{ok, SubFiles} = file:list_dir(Dir),
 		[filename:join(Dir, SubFile) || SubFile <- SubFiles].
@@ -29,8 +31,8 @@ remove_recursive(Path) ->
 				file:del_dir(Path)
 		end.
 open_for_write(Filename) ->
-	filelib:ensure_dir(Filename),
-  case file:open(Filename, [exclusive, write, binary, raw]) of
+    filelib:ensure_dir(Filename),
+    case file:open(Filename, [exclusive, write, binary, raw]) of
 		{ok, Fd} -> {ok,Fd};
 		Error -> Error
 	end.			
@@ -50,6 +52,18 @@ file_size(Filename)->
               end;
          Error-> Error
     end.
-    
-                
-  
+
+hash_to_path(Level,HashString)->
+    {Path,_Rest} = hash_to_path(Level,HashString,[]),
+    filename:join(Path).
+hash_to_fullname(Level,HashString)->
+    {Path,Rest} = hash_to_path(Level,HashString,[]),
+    HashPath = filename:join(Path),
+    filename:join([HashPath,Rest]).
+
+hash_to_path(0,Rest,Acc)->
+    {lists:reverse(Acc),Rest};
+hash_to_path(_N,[],_Acc)->
+    erlang:throw({error,file_path_too_deep});
+hash_to_path(N,[H1,H2|T],Acc)->
+    hash_to_path(N-1,T,[[H1,H2] | Acc]).    
--- src/ai_strings.erl
+++ src/ai_strings.erl
@@ -1,6 +1,6 @@
 -module(ai_strings).
 -export([to_string/1,to_string/2]).
--export([hash_to_string/3,md5_string/2]).
+-export([hash_to_string/3,md5_string/2,sha_string/2,sha256_string/2,sha512_string/2]).
 
 to_string(Val) when is_integer(Val) -> erlang:integer_to_binary(Val);
 to_string(Val) when is_float(Val) -> erlang:list_to_binary(io_lib:format("~.2f", [Val]));
@@ -14,6 +14,16 @@ to_string(Format,Val) ->
 md5_string(Data,Case)->
 	Hash = crypto:hash(md5, Data),
 	hash_to_string(Hash,128,Case).
+sha_string(Data,Case)->
+	Hash = crypto:hash(sha,Data),
+	hash_to_string(Hash,160,Case).
+sha256_string(Data,Case) ->
+	Hash = crypto:hash(sha256,Data),
+	hash_to_string(Hash,256,Case).
+sha512_string(Data,Case)->
+	Hash = crypto:hash(sha512,Data),
+	hash_to_string(Hash,512,Case).
+
 hash_to_string(Hash,Len,Case) ->
 	<<S:Len/big-unsigned-integer>> = Hash,
 	FormatStr = hash_format(Len,Case),
