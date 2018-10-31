815e61e679c92def452431006ea3e0d1f1d7526d
David.Gao <david.alpha.fox@gmail.com>
2018-10-31 16:09:53 +0800

    add prefix and find on ai_strings to support otp 19.3

--- src/ai_strings.erl
+++ src/ai_strings.erl
@@ -1,7 +1,7 @@
 -module(ai_strings).
 -export([to_string/1,to_string/2]).
 -export([hash_to_string/3,md5_string/2,sha_string/2,sha256_string/2,sha512_string/2]).
-
+-export([prefix/2,find/3]).
 to_string(Val) when is_integer(Val) -> erlang:integer_to_binary(Val);
 to_string(Val) when is_float(Val) -> erlang:list_to_binary(io_lib:format("~.2f", [Val]));
 to_string(Val) when is_atom(Val) -> erlang:atom_to_binary(Val);
@@ -29,7 +29,166 @@ hash_to_string(Hash,Len,Case) ->
 	FormatStr = hash_format(Len,Case),
 	erlang:list_to_binary(lists:flatten(io_lib:format(FormatStr,[S]))).
 
+-spec prefix(String::unicode:chardata(), Prefix::unicode:chardata()) ->
+                    'nomatch' | unicode:chardata().
+prefix(Str, Prefix0) ->
+    Result = 
+		case unicode:characters_to_list(Prefix0) of
+            [] -> Str;
+            Prefix -> prefix_1(Str, Prefix)
+        end,
+    case Result of
+        [] when is_binary(Str) -> <<>>;
+        Res -> Res
+    end.
 
+prefix_1(Cs0, [GC]) ->
+    case unicode_util:gc(Cs0) of
+        [GC|Cs] -> Cs;
+        _ -> nomatch
+    end;
+prefix_1([CP|Cs], [Pre|PreR]) when is_integer(CP) ->
+    case CP =:= Pre of
+        true -> prefix_1(Cs,PreR);
+        false -> nomatch
+    end;
+prefix_1(<<CP/utf8, Cs/binary>>, [Pre|PreR]) ->
+    case CP =:= Pre of
+        true -> prefix_1(Cs,PreR);
+        false -> nomatch
+    end;
+prefix_1(Cs0, [Pre|PreR]) ->
+    case unicode_util:cp(Cs0) of
+        [Pre|Cs] ->  prefix_1(Cs,PreR);
+        _ -> nomatch
+end.
+-spec find(String, SearchPattern, Dir) -> unicode:chardata() | 'nomatch' when
+      String::unicode:chardata(),
+      SearchPattern::unicode:chardata(),
+      Dir::direction().
+find(String, "", _) -> String;
+find(String, <<>>, _) -> String;
+find(String, SearchPattern, leading) ->
+    find_l(String, unicode:characters_to_list(SearchPattern));
+find(String, SearchPattern, trailing) ->
+	find_r(String, unicode:characters_to_list(SearchPattern), nomatch).
+find_l([C1|Cs]=Cs0, [C|_]=Needle) when is_integer(C1) ->
+    case C1 of
+        C ->
+            case prefix_1(Cs0, Needle) of
+                nomatch -> find_l(Cs, Needle);
+                _ -> Cs0
+            end;
+        _ ->
+            find_l(Cs, Needle)
+    end;
+find_l([Bin|Cont0], Needle) when is_binary(Bin) ->
+    case bin_search_str(Bin, 0, Cont0, Needle) of
+        {nomatch, _, Cont} ->
+            find_l(Cont, Needle);
+        {_Before, Cs, _After} ->
+            Cs
+    end;
+find_l(Cs0, [C|_]=Needle) when is_list(Cs0) ->
+    case unicode_util:cp(Cs0) of
+        [C|Cs] ->
+            case prefix_1(Cs0, Needle) of
+                nomatch -> find_l(Cs, Needle);
+                _ -> Cs0
+            end;
+        [_C|Cs] ->
+            find_l(Cs, Needle);
+        [] -> nomatch
+    end;
+find_l(Bin, Needle) ->
+    case bin_search_str(Bin, 0, [], Needle) of
+        {nomatch,_,_} -> nomatch;
+        {_Before, [Cs], _After} -> Cs
+    end.
+
+find_r([Cp|Cs]=Cs0, [C|_]=Needle, Res) when is_integer(Cp) ->
+    case Cp of
+        C ->
+            case prefix_1(Cs0, Needle) of
+                nomatch -> find_r(Cs, Needle, Res);
+                _ -> find_r(Cs, Needle, Cs0)
+            end;
+        _ ->
+            find_r(Cs, Needle, Res)
+    end;
+find_r([Bin|Cont0], Needle, Res) when is_binary(Bin) ->
+    case bin_search_str(Bin, 0, Cont0, Needle) of
+        {nomatch,_,Cont} ->
+            find_r(Cont, Needle, Res);
+        {_, Cs0, _} ->
+            [_|Cs] = unicode_util:gc(Cs0),
+            find_r(Cs, Needle, Cs0)
+    end;
+find_r(Cs0, [C|_]=Needle, Res) when is_list(Cs0) ->
+    case unicode_util:cp(Cs0) of
+        [C|Cs] ->
+            case prefix_1(Cs0, Needle) of
+                nomatch -> find_r(Cs, Needle, Res);
+                _ -> find_r(Cs, Needle, Cs0)
+            end;
+        [_C|Cs] ->
+            find_r(Cs, Needle, Res);
+        [] -> Res
+    end;
+find_r(Bin, Needle, Res) ->
+    case bin_search_str(Bin, 0, [], Needle) of
+        {nomatch,_,_} -> Res;
+        {_Before, [Cs0], _After} ->
+            <<_/utf8, Cs/binary>> = Cs0,
+            find_r(Cs, Needle, Cs0)
+end.
+
+bin_search_str(Bin0, Start, [], SearchCPs) ->
+    Compiled = binary:compile_pattern(unicode:characters_to_binary(SearchCPs)),
+    bin_search_str_1(Bin0, Start, Compiled, SearchCPs);
+bin_search_str(Bin0, Start, Cont, [CP|_]=SearchCPs) ->
+    First = binary:compile_pattern(<<CP/utf8>>),
+    bin_search_str_2(Bin0, Start, Cont, First, SearchCPs).
+
+bin_search_str_1(Bin0, Start, First, SearchCPs) ->
+    <<_:Start/binary, Bin/binary>> = Bin0,
+    case binary:match(Bin, First) of
+        nomatch -> {nomatch, byte_size(Bin0), []};
+        {Where0, _} ->
+            Where = Start+Where0,
+            <<Keep:Where/binary, Cs0/binary>> = Bin0,
+            case prefix_1(Cs0, SearchCPs) of
+                nomatch ->
+                    <<_/utf8, Cs/binary>> = Cs0,
+                    KeepSz = byte_size(Bin0) - byte_size(Cs),
+                    bin_search_str_1(Bin0, KeepSz, First, SearchCPs);
+                [] ->
+                    {Keep, [Cs0], <<>>};
+                Rest ->
+                    {Keep, [Cs0], Rest}
+            end
+    end.
+
+bin_search_str_2(Bin0, Start, Cont, First, SearchCPs) ->
+    <<_:Start/binary, Bin/binary>> = Bin0,
+    case binary:match(Bin, First) of
+        nomatch -> {nomatch, byte_size(Bin0), Cont};
+        {Where0, _} ->
+            Where = Start+Where0,
+            <<Keep:Where/binary, Cs0/binary>> = Bin0,
+            [GC|Cs]=unicode_util:gc(Cs0),
+            case prefix_1(stack(Cs0,Cont), SearchCPs) of
+                nomatch when is_binary(Cs) ->
+                    KeepSz = byte_size(Bin0) - byte_size(Cs),
+                    bin_search_str_2(Bin0, KeepSz, Cont, First, SearchCPs);
+                nomatch ->
+                    {nomatch, Where, stack([GC|Cs],Cont)};
+                [] ->
+                    {Keep, [Cs0|Cont], <<>>};
+                Rest ->
+                    {Keep, [Cs0|Cont], Rest}
+            end
+end.
 %%%
 %%% private
 %%%
