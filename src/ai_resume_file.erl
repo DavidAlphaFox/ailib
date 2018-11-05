36647cb8fdb0d7a91fc4ecec9408ccbc5e841434
David.Gao <david.alpha.fox@gmail.com>
2018-11-05 14:13:39 +0800

    auto truncate resume file

--- src/ai_resume_file.erl
+++ src/ai_resume_file.erl
@@ -4,12 +4,14 @@
 
 -export([open/1]).
 
+-export([file_length/1,received/1,etag/1,last_modified/1]).
+
 -define(RANGE_SIZE_BYTES, 8).
 -define(LENGTH_SIZE_BYTES, 8).
 -define(ETAG_SIZE_BYTES, 2).
 -define(LAST_MODIFIED_SIZE_BYTES, 2).
 
--record(ai_resume_file,{fd,etag,last_modified,range,length}).
+-record(ai_resume_file,{fd,etag,last_modified,received,length}).
 
 
 open(Filenmae)->
@@ -29,12 +31,31 @@ write_extend_header(Fd)->
     Ext = <<0:64/big-unsigned-integer,0:64/big-unsigned-integer,0:16/big-unsigned-integer,0:16>>,
     case ai_blob_file:write_extend_header(Fd,Ext) of 
         ok -> 
-            {ok,#ai_resume_file{fd = Fd, etag = undefined,last_modified = undefined,range = 0,length =0}};
+            {ok,#ai_resume_file{fd = Fd, etag = undefined,last_modified = undefined,received = 0,length =0}};
         Error ->
             ai_blob_file:close(Fd),
             Error 
     end.
-wrap_fd(Fd,<<Range:64/big-unsigned-integer,Length:64/big-unsigned-integer,
+wrap_fd(Fd,<<Received:64/big-unsigned-integer,Length:64/big-unsigned-integer,
             EtagSize:16/big-unsigned-integer,LastModifiedSize:16/big-unsigned-integer,Rest/binary>>)->
     <<Etag:EtagSize/binary,LastModified:LastModifiedSize/binary,_Other/binary>> = Rest,
-    {ok,#ai_resume_file{fd = Fd,etag = Etag,last_modified = LastModified,range = Range,length = Length}}.
+    {ok,BlobSize} = ai_blob_file:data_size(Fd),
+    if 
+        BlobSize > Received ->
+            case ai_blob_file:truncate(Fd,Received) of 
+                {ok,NewFd} ->
+                      {ok,#ai_resume_file{fd = NewFd,etag = Etag,last_modified = LastModified,
+                        received = Received,length = Length}};
+                Error ->
+                    ai_blob_file:close(Fd),
+                    Error 
+            end;
+        true ->
+            {ok,#ai_resume_file{fd = Fd,etag = Etag,last_modified = LastModified,
+                        received = Received,length = Length}}
+    end.
+
+file_length(#ai_resume_file{length = Length}) -> {ok,Length}.
+received(#ai_resume_file{received = Received}) -> {ok,Received}.
+etag(#ai_resume_file{etag = Etag}) -> {ok,Etag}.
+last_modified(#ai_resume_file{last_modified = LastModified}) -> {ok,LastModified}.
