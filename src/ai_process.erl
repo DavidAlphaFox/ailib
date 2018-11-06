f5c85c6777ff21bc143d4171efb33d094c748a7b
David Gao <david.alpha.fox@gmail.com=>
2018-11-06 23:32:25 +0800

    monitor support on ets tab;e

--- src/ai_process.erl
+++ src/ai_process.erl
@@ -1,18 +1,27 @@
 -module(ai_process).
 -export([demonitor_process/2,monitor_process/2]).
+-export([least_busy_pid/1]).
 
 monitor_process(undefined,Monitors) -> Monitors;
-monitor_process(Pid,Monitors)->
+monitor_process(Pid,Monitors) when erlang:is_map(Monitors)->
     case maps:get(Pid,Monitors,undefined) of 
         undefined ->
             MRef = erlang:monitor(process,Pid),
             M1 = maps:put(Pid,MRef,Monitors),
             maps:put(MRef,Pid,M1);
         _ -> Monitors
-    end.
+    end;
+monitor_process(Pid,Monitors)->
+    case ets:match_object(Monitors, {Pid,'_'}) of
+        [] ->
+            MRef = erlang:monitor(process, Pid),
+            ets:insert(Monitors, {Pid, MRef});
+        _ -> ok
+    end,
+    Monitors.
 
 demonitor_process(undefined,Monitors)-> Monitors;
-demonitor_process(Pid,Monitors) when erlang:is_pid(Pid)-> 
+demonitor_process(Pid,Monitors) when erlang:is_map(Monitors) and erlang:is_pid(Pid) -> 
     case maps:get(Pid,Monitors,undefined) of 
         undefined -> Monitors;
         MRef -> 
@@ -20,11 +29,43 @@ demonitor_process(Pid,Monitors) when erlang:is_pid(Pid)->
             M1 = maps:remove(MRef,Monitors),
             maps:remove(Pid,M1)
     end;
-demonitor_process(MRef,Monitors) when erlang:is_reference(MRef)->
+demonitor_process(MRef,Monitors) when erlang:is_map(Monitors) and erlang:is_reference(MRef)->
     erlang:demonitor(MRef),
     case maps:get(MRef,Monitors,undefined) of 
         undefined -> Monitors;
         Pid ->
             M1 = maps:remove(MRef,Monitors),
             maps:remove(Pid,M1)
+    end;
+demonitor_process(Pid,Monitors) when erlang:is_pid(Pid)->
+    case ets:match_object(Monitors,{Pid,'_'}) of
+        [{Pid,MRef}] ->
+            erlang:demonitor(MRef),
+            ets:delete(Monitors,Pid);
+         [] -> ok
+   end,
+   Monitors;
+demonitor_process(MRef,Monitors) when erlang:is_reference(MRef)->
+        case ets:match_object(Monitors,{'_',MRef}) of
+        [{Pid,MRef}] ->
+            erlang:demonitor(MRef),
+            ets:delete(Monitors,Pid);
+         [] -> ok
+   end,
+   Monitors.
+
+least_busy_pid(Pids) ->
+    Members = lists:map(fun(Pid) ->
+                [
+                    {message_queue_len, Messages},
+                    {stack_size, StackSize}
+                ] = erlang:process_info(Pid, [message_queue_len, stack_size]),
+                {Pid, Messages, StackSize}
+            end, Pids),
+    SortedMembers = lists:keysort(2, lists:keysort(3, Members)),
+    case SortedMembers of
+        [{Pid, _Messages, _StackSize}] -> Pid;
+        [{Pid, _Messages, _StackSize} | _Tail] -> Pid;
+        _ -> {error, empty_process_group}
     end.
+        
