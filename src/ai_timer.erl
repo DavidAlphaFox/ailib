f8ecd731067cfc72a5efeec33aa9dd541a35838e
David.Gao <david.alpha.fox@gmail.com>
2018-11-09 17:10:30 +0800

    timer operation

--- src/ai_timer.erl
+++ src/ai_timer.erl
@@ -1,29 +1,26 @@
 -module(ai_timer).
 -export([new/0]).
 -export([start/3,restart/1,cancel/1]).
--export([is_canceled/2]).
 
 
--record(ai_timer,{timeout,msg,prev,current}).
+-record(ai_timer,{timeout,msg,ref}).
 
-new()-> #ai_timer{timeout = infinity,msg = undefined,prev = undefined,current = undefined}.
+new()-> #ai_timer{timeout = infinity,msg = undefined,ref = undefined}.
 
-start(Timeout,TimeoutMsg,#ai_timer{current = PrevRef})->
+start(Timeout,TimeoutMsg,#ai_timer{ref = PrevRef})->
     if PrevRef == undefined -> ok;
        true -> erlang:cancel_timer(PrevRef)
     end,
     Ref = erlang:send_after(Timeout, self(), TimeoutMsg),
-    #ai_timer{timeout = Timeout,msg = TimeoutMsg, prev = PrevRef,current = Ref}.
+    #ai_timer{timeout = Timeout,msg = TimeoutMsg,ref = Ref}.
 restart(#ai_timer{timeout = Timeout, msg = TimeoutMsg}  = Timer)->
     if 
         Timeout  == infinity -> {error,infinity};
         TimeoutMsg == undefined -> {error,unknown_message};
         true -> start(Timeout,TimeoutMsg,Timer)
     end.
-cancel(#ai_timer{current = Ref} = Timer)->
+cancel(#ai_timer{ref = Ref} = Timer)->
 	if Ref == undefined -> ok;
        true -> erlang:cancel_timer(Ref)
     end,
-    Timer#ai_timer{prev = Ref,current = undefined}.
-is_canceled(Ref,#ai_timer{current = Ref}) -> false;
-is_canceled(_Ref,_Timer) -> true.
+    Timer#ai_timer{ref = undefined}.
