eafcd5654b0565f5d3b94691f798267df5050dec
David Gao <david.alpha.fox@gmail.com=>
2018-11-16 01:00:54 +0800

    auto start

--- src/ailib_app.erl
+++ src/ailib_app.erl
@@ -5,6 +5,9 @@
 -export([stop/1]).
 
 start(_Type, _Args) ->
+	application:ensure_started(crypto),
+    application:ensure_started(poolboy),
+    application:ensure_started(cowlib),
 	ailib_sup:start_link().
 
 stop(_State) ->
