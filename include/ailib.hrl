5d813b6e96c6ccca210e05c0e0474c270ee5cee5
david.gao <david.alpha.fox@gmail.com>
2018-11-15 21:41:32 +0800

    remove file lib from ailib

--- include/ailib.hrl
+++ include/ailib.hrl
@@ -9,14 +9,3 @@
                      zone :: binary(),
                      utc_diff :: {integer(),integer()}
                     }).
-
--record(ai_http_cache,{ 
-                    key :: binary(), 
-                    cache_key :: term(),
-                    date :: binary(),
-                    etag :: binary(),
-                    strategy :: atom(), %% no_cache | age
-                    age :: integer(),
-                    last_modified :: binary(),
-                    headers :: term()
-                }).
