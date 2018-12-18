46c969181d8d0a18a71daaad25b618c2220cf63e
David Gao <david.alpha.fox@gmail.com=>
2018-12-18 23:04:40 +0800

    add while function

--- src/ai_function.erl
+++ src/ai_function.erl
@@ -1,13 +1,17 @@
 -module(ai_function).
 -export([run_mfa/1,run_mfa/2,run_catch_mfa/1, run_catch_mfa/2]).
--export([lookup_func/3,apply_func/4]).
+-export([lookup_fun/3,apply_fun/4]).
+-export([while/2]).
 
+-spec run_mfa({module(),atom(),[term()]}|{function(),[term()]})->term().
 run_mfa({M,F,A})-> erlang:apply(M,F,A);
 run_mfa({F,A}) -> erlang:apply(F,A).
 
+-spec run_mfa({module(),atom(),[term()]}|{function(),[term()]},[term()])->term().
 run_mfa({M,F,A},Others) -> erlang:apply(M,F,A ++ Others);
 run_mfa({F,A},Others) -> erlang:apply(F,A ++ Others).
 
+-spec run_catch_mfa({module(),atom(),[term()]}|{function(),[term()]}) ->term().
 run_catch_mfa(MFA)->
 	try 
 		run_mfa(MFA)
@@ -15,6 +19,7 @@ run_catch_mfa(MFA)->
 		Error:Reason -> {exception,Error,Reason}
 	end.
 
+-spec run_catch_mfa({module(),atom(),[term()]}|{function(),[term()]},[term()]) ->term().
 run_catch_mfa(MFA,Others)->
 	try 
 		run_mfa(MFA,Others)
@@ -24,18 +29,23 @@ run_catch_mfa(MFA,Others)->
 		
 			
 
-
-lookup_func(Mod,Func,ArgCount) when erlang:is_list(Func)->
-	FuncBin = erlang:list_to_binary(Func),
-	lookup_func(Mod,FuncBin,ArgCount);
-lookup_func(Mod,Func,ArgCount)->
+-spec lookup_fun(atom(),list()|binary(),integer())-> boolean().
+lookup_fun(Mod,Fun,ArgCount) when erlang:is_list(Fun)->
+	FunBin = erlang:list_to_binary(Fun),
+	lookup_fun(Mod,FunBin,ArgCount);
+lookup_fun(Mod,Fun,ArgCount)->
 	Exports = Mod:module_info(exports),
-	lists:filter(fun(FuncInMod)->
-					{FuncName,AC} = FuncInMod,
-					FuncNameBin = erlang:atom_to_binary(FuncName,latin1),
-					FuncNameBin =:= Func andalso AC == ArgCount
+	lists:filter(fun(FunInMod)->
+					{FunName,AC} = FunInMod,
+					FunNameBin = erlang:atom_to_binary(FunName,latin1),
+					FunNameBin =:= Fun andalso AC == ArgCount
 				end,Exports).
-apply_func(Mod,Func,ArgCount,Args)->
-	Res = lookup_func(Mod,Func,ArgCount),
+-spec apply_fun(module(),atom(),arity(),[term()])->term().
+apply_fun(Mod,Fun,ArgCount,Args)->
+	Res = lookup_fun(Mod,Fun,ArgCount),
 	[{Fun,_AC}] = Res,
 	erlang:apply(Mod,Fun,Args).
+
+-spec while({boolean(), term()}, fun((term()) -> {boolean(), term()})) -> term().
+while({true, Value}, Fun) -> while(Fun(Value), Fun);
+while({false, Value}, _Fun) -> Value.
