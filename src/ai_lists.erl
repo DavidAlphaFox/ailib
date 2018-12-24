f47c114bda42faed9238c9e00758955fb8e265b5
David.Gao <david.alpha.fox@gmail.com>
2018-12-24 15:07:51 +0800

    ai_lists foldl foldr

--- src/ai_lists.erl
+++ src/ai_lists.erl
@@ -1,5 +1,6 @@
 -module(ai_lists).
 -export([search/2,break_on_error/1,run_pipe/2,run_pipe/1]).
+-export([foldl/3,foldr/3]).
 -spec search(Pred, List) -> {value, Value} | false when
       Pred :: fun((T) -> boolean()),
       List :: [T],
@@ -30,3 +31,19 @@ run_pipe([H|T],Args)->
     R = ai_function:run_mfa(H,Args),
     run_pipe(T,[R]);
 run_pipe([],[R]) -> R.
+
+foldl(_Fun,Acc,[])-> Acc;
+foldl(Fun,Acc,L) when erlang:is_function(Fun)-> lists:foldl(Fun,Acc,L);
+foldl({M,F},Acc,L)->
+    foldl(M,F,Acc,L).
+foldl(_M,_F,Acc,[])-> Acc;
+foldl(M,F,Acc,[H|T])->
+    Acc0 = M:F(H,Acc),
+    foldl(M,F,Acc0,T).
+foldr(_Fun,Acc,[])->Acc;
+foldr(Fun,Acc,L) when erlang:is_function(Fun)->
+    L0 = lists:reverse(L),
+    lists:foldl(Fun,Acc,L0);
+foldr({M,F},Acc,L)->
+    L0 = lists:reverse(L),
+    foldl(M,F,Acc,L0).
