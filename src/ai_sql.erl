0721cbb05a5884d7e3ab8f45d6a35ce14b2f6e78
David.Gao <david.alpha.fox@gmail.com>
2018-12-29 18:11:35 +0800

    fix field escape error

--- src/ai_sql.erl
+++ src/ai_sql.erl
@@ -253,7 +253,7 @@ placeholder({Prefix, N}) ->
 escape_field({'as',Field,ASField})->
     F = escape_field(Field),
     AF = escape_field(ASField),
-    <<" \"",F/binary," AS ",AF/binary,"\" ">>;
+    <<" ",F/binary," AS ",AF/binary," ">>;
 escape_field(Field) -> 
     F = ai_string:to_string(Field),
     if 
