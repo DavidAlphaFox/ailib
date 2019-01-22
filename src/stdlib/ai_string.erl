5ba2d20f5ba4662fc612c5eec689bd3fe9f0b451
David.Gao <david.alpha.fox@gmail.com>
2019-01-22 09:53:15 +0800

    drop support on otp 19

--- src/stdlib/ai_string.erl
+++ src/stdlib/ai_string.erl
@@ -4,12 +4,10 @@
 -export([to_integer/1,to_boolean/1]).
 -export([md5_string/2,sha_string/2,sha256_string/2,sha512_string/2]).
 -export([string_to_hex/2,hash_to_string/3]).
--export([prefix/2,find/3,slice/2,slice/3,join/2]).
--export([atom_suffix/3]).
+-export([atom_suffix/3,join/2]).
 -export([dynamic_module/2]).
 -export([html_escape/1,html_unescape/1]).
 -export([to_iolist/1]).
--export([to_upper/1,to_lower/1]).
 
 -define(HTML_ESCAPE,[
 		{"\\&","\\&amp"},{"<","\\&lt;"},{">","\\&gt;"},
@@ -123,142 +121,12 @@ atom_suffix(Name,false)-> erlang:list_to_atom(Name);
 atom_suffix(Name,true)->erlang:list_to_existing_atom(Name).
 
 
--spec prefix(String::unicode:chardata(), Prefix::unicode:chardata()) ->
-                    'nomatch' | unicode:chardata().
--ifdef(OTP_RELEASE).
-prefix(Str, Prefix0) -> string:prefix(Str,Prefix0).
--else.
-prefix(Str, Prefix0) -> ai_string_compat:prefix(Str,Prefix0).
--endif.
-
--spec find(String, SearchPattern, Dir) -> unicode:chardata() | 'nomatch' when
-      String::unicode:chardata(),
-      SearchPattern::unicode:chardata(),
-      Dir::atom().
--ifdef(OTP_RELEASE).
-find(String,SearchPattern,Dir)-> string:find(String,SearchPattern,Dir).
--else.
-find(String,SearchPattern,Dir)-> ai_string_compat:find(String,SearchPattern,Dir).
--endif.
-
--spec slice(String, Start) -> Slice when
-      String::unicode:chardata(),
-      Start :: non_neg_integer(),
-      Slice :: unicode:chardata().
--ifdef(OTP_RELEASE).
-slice(String,Start)-> string:slice(String,Start).
--else.
-slice(String,Start) -> ai_string_compat:slice(String,Start).
--endif.
-
--spec slice(String, Start, Length) -> Slice when
-      String::unicode:chardata(),
-      Start :: non_neg_integer(),
-      Length :: 'infinity' | non_neg_integer(),
-      Slice :: unicode:chardata().
--ifdef(OTP_RELEASE).
-slice(String,Start,Length)-> string:slice(String,Start,Length).
--else.
-slice(String,Start,Length) -> ai_string_compat:slice(String,Start,Length).
--endif.
-
 join([], _) -> <<>>;
 join([H|T],Sep)->
 	SepBin = to_string(Sep),
 	Fun = fun(B)-> Str = to_string(B),<<SepBin/binary,Str/binary>> end,
 	L = [ Fun(B) || B <- T ],
     lists:foldl(fun(I,Acc)-> <<Acc/binary,I/binary>> end,to_string(H),L).
-
-	-spec to_lower(binary()|atom()|list()) -> binary().
-
-
-
-
-to_lower(L) when is_binary(L) ->
-	OTPRelease = erlang:list_to_integer(erlang:system_info(otp_release)),
-	if 
-		OTPRelease >= 20 -> string:lowercase(L);
-		true -> << << (char_to_lower(C)) >> || << C >> <= L >>
-	end;
-to_lower(L) ->
-  to_lower(to_string(L)).
-
--spec to_upper(binary()|atom()|list()) -> binary().
-to_upper(U) when is_binary(U)->
-	OTPRelease = erlang:list_to_integer(erlang:system_info(otp_release)),
-	if 
-		OTPRelease >= 20 -> string:uppercase(U);
-		true ->   << << (char_to_upper(C)) >> || << C >> <= U >>
-	end;
-to_upper(L) ->
-  to_upper(to_string(L)).
-
-%%%
-%%% private
-%%%
-
-%% @doc Convert [A-Z] characters to lowercase.
-%% @end
-%% We gain noticeable speed by matching each value directly.
--spec char_to_lower(char()) -> char().
-char_to_lower($A) -> $a;
-char_to_lower($B) -> $b;
-char_to_lower($C) -> $c;
-char_to_lower($D) -> $d;
-char_to_lower($E) -> $e;
-char_to_lower($F) -> $f;
-char_to_lower($G) -> $g;
-char_to_lower($H) -> $h;
-char_to_lower($I) -> $i;
-char_to_lower($J) -> $j;
-char_to_lower($K) -> $k;
-char_to_lower($L) -> $l;
-char_to_lower($M) -> $m;
-char_to_lower($N) -> $n;
-char_to_lower($O) -> $o;
-char_to_lower($P) -> $p;
-char_to_lower($Q) -> $q;
-char_to_lower($R) -> $r;
-char_to_lower($S) -> $s;
-char_to_lower($T) -> $t;
-char_to_lower($U) -> $u;
-char_to_lower($V) -> $v;
-char_to_lower($W) -> $w;
-char_to_lower($X) -> $x;
-char_to_lower($Y) -> $y;
-char_to_lower($Z) -> $z;
-char_to_lower(Ch) -> Ch.
-
-%% @doc Convert [a-z] characters to uppercase.
--spec char_to_upper(char()) -> char().
-char_to_upper($a) -> $A;
-char_to_upper($b) -> $B;
-char_to_upper($c) -> $C;
-char_to_upper($d) -> $D;
-char_to_upper($e) -> $E;
-char_to_upper($f) -> $F;
-char_to_upper($g) -> $G;
-char_to_upper($h) -> $H;
-char_to_upper($i) -> $I;
-char_to_upper($j) -> $J;
-char_to_upper($k) -> $K;
-char_to_upper($l) -> $L;
-char_to_upper($m) -> $M;
-char_to_upper($n) -> $N;
-char_to_upper($o) -> $O;
-char_to_upper($p) -> $P;
-char_to_upper($q) -> $Q;
-char_to_upper($r) -> $R;
-char_to_upper($s) -> $S;
-char_to_upper($t) -> $T;
-char_to_upper($u) -> $U;
-char_to_upper($v) -> $V;
-char_to_upper($w) -> $W;
-char_to_upper($x) -> $X;
-char_to_upper($y) -> $Y;
-char_to_upper($z) -> $Z;
-char_to_upper(Ch) -> Ch.
-
 hex_format(Size,Case)-> 
 	Base = "~X.16.0",
 	F = 
