126d4e856583d8c2a131baac0629274ad8dfa38c
David Gao <david.alpha.fox@gmail.com>
2022-03-27 20:43:35 +0800

    rewrite ai_id to ai_snowflake

--- src/process/ailib_snowflake.erl
+++ src/process/ailib_snowflake.erl
@@ -1,28 +1,29 @@
 % The id server will generate a 64-bit integer base on utc time.
 % The id is k-order shortable
-% 
 %
-% Bits                      Description      
+%
+% Bits   Description
 % 1      Signeddness flag,always 0.Because thrift only supports signed 64-bit integer And I don't want a negtive integer.
-% 41     Unix timestamp,down to the millisecond      
+% 41     Unix timestamp,down to the millisecond
 % 5      Top 5 bits of node number
-% 5      then 5 bits of partition number 
+% 5      then 5 bits of partition number
 % 12     Per-partition static increasing counter
 % +------------------+------------------+--------------+
 % |0|1     ...     41|42      ...     51|52   ...    63|
-% +------------------+------------------+--------------+     
+% +------------------+------------------+--------------+
 % |0| Unix Timestamp | Partition Number |    Counter   |
 %
 
 
--module(ai_id).
+-module(ailib_snowflake).
 
 -behaviour(gen_server).
 
 -export([start_link/1]).
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
          terminate/2, code_change/3]).
--export ([next_id/0,next_id/1]).
+
+-export ([next/1]).
 
 %-define (EPOCH,1356998400974295).%2013-1-1 0:0:0 {1356,998400,974295} microseconds
 
@@ -37,19 +38,20 @@
 
 start_link(Args)->
   Partition = proplists:get_value(partition,Args,0),
-  Name = proplists:get_value(name,Args,?MODULE),
+  Name = server_name(Partition),
   gen_server:start_link({local,Name},?MODULE,Partition,[]).
-      
+
 %%%
 %%% API
 %%%
--spec next_id() -> {error, backwards_clock} | {ok, integer()}.
-next_id()->
-  gen_server:call(?MODULE,next_id).
+-spec next(integer() | pid()) -> {error, backwards_clock} | {ok, integer()}.
+next(ParOrPid)->
+  ServerName =
+    if  erlang:is_pid(ParOrPid) -> ParOrPid;
+        true -> server_name(ParOrPid)
+    end,
+  gen_server:call(ServerName,next_id).
 
--spec next_id(Server::atom()) -> {error, backwards_clock} | {ok, integer()}.
-next_id(Server)->
-  gen_server:call(Server,next_id).
 %%%
 %%% gen_server callback
 %%%
@@ -108,3 +110,6 @@ next_id(Millis, Partition, Seq) ->
     <<Integer:64/big-unsigned-integer>> = <<0:1, Millis:41/integer-unsigned,
                                             Partition:10/bits, Seq:12/integer-unsigned>>,
     {ok,Integer}.
+
+server_name(Partition)->
+  ailib_atom:suffix(?MODULE, <<"_",Partition/integer>>).
